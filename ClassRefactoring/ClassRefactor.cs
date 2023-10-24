89b0bf1c5aee68fad1ed73dc2163a84913f62a64
Sean Slicer <722sas17@gmail.com>
2023-10-24 06:28:10 -0500

    Loads in own class

--- ClassRefactoring/ClassRefactor.cs
+++ ClassRefactoring/ClassRefactor.cs
@@ -19,6 +19,29 @@ namespace DeveloperSample.ClassRefactoring
         Coconut
     }
 
+    public abstract class Load
+    {
+        public abstract double ApplyLoad(double baseAirspeedVelocity);
+    }
+
+    public class NoLoad : Load
+    {
+        public override double ApplyLoad(double baseAirspeedVelocity)
+        {
+            return baseAirspeedVelocity;
+        }
+    }
+
+    public class CoconutLoad : Load
+    {
+        public const double CoconutEffect = 4D;
+        public override double ApplyLoad(double baseAirspeedVelocity)
+        {
+            double newAirspeedVelocity = baseAirspeedVelocity - CoconutEffect;
+            return newAirspeedVelocity;
+        }
+    }
+
     public class SwallowFactory
     {
         public Swallow GetSwallow(SwallowType swallowType) => swallowType switch
@@ -32,21 +55,20 @@ namespace DeveloperSample.ClassRefactoring
     public abstract class Swallow : IBird
     {
         protected double BaseAirspeedVelocity { get; set; } = 0;
-        public SwallowLoad Load { get; private set; }
+        public Load Load { get; private set; } = new NoLoad();
 
         public void ApplyLoad(SwallowLoad load)
         {
-            Load = load;
+            Load = load switch
+            {
+                SwallowLoad.Coconut => new CoconutLoad(),
+                _ => throw new ArgumentException("Bad load type.")
+            };
         }
 
         public double GetAirspeedVelocity()
         {
-            return Load switch 
-            {
-                SwallowLoad.None => BaseAirspeedVelocity,
-                SwallowLoad.Coconut => BaseAirspeedVelocity -4D,
-                _ => throw new InvalidOperationException(),
-            };
+            return Load.ApplyLoad(BaseAirspeedVelocity);
         }
     }
 
@@ -54,15 +76,15 @@ namespace DeveloperSample.ClassRefactoring
     {
         public AfricanSwallow()
         {
-        BaseAirspeedVelocity = 22;
-        } 
+            BaseAirspeedVelocity = 22D;
+        }
     }
 
     public class EuropeanSwallow : Swallow
     {
         public EuropeanSwallow()
         {
-        BaseAirspeedVelocity = 20;
+            BaseAirspeedVelocity = 20D;
         }
     }
 }
