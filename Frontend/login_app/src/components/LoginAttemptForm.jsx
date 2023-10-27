a4a8b51a971833db5cb252d78b233137d5d7e681
Sean Slicer <seanslicer@gmail.com>
2023-10-27 10:10:17 -0500

    react query revert (time constraints)

--- Frontend/login_app/src/components/LoginAttemptForm.jsx
+++ Frontend/login_app/src/components/LoginAttemptForm.jsx
@@ -1,14 +1,11 @@
 import LoginForm from "./Auth/LoginForm";
 import LoginAttemptList from "./Auth/LoginAttemptList";
 import React, { useState } from "react";
-import { useStoreLoginAttempt } from "../hooks/auth/useStoreLoginAttempt";
 
 const LoginAttemptForm = () => {
-  const storeLoginAttempt = useStoreLoginAttempt();
   const [loginAttempts, setLoginAttempts] = useState([]);
 
   const handleLoginAttempt = ({ username, password }) => {
-    storeLoginAttempt.mutate({ username, password });
     const newLoginAttempts = [...loginAttempts, { username, password }];
     setLoginAttempts(newLoginAttempts);
   };
--- Frontend/login_app/src/hooks/auth/useStoreLoginAttempt.js
+++
@@ -1,7 +0,0 @@
-import { useMutation } from "react-query";
-
-export const useStoreLoginAttempt = () => {
-  return useMutation(({ username, password }) => {
-    // Here you would typically make a POST request to your API to store the login attempt
-  });
-};
